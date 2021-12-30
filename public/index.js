2d84e0a9c6fb9f85a47fbc905fb5b9c25ca7403a
¨DevSeba78¨ <villarroelssebastian@gmail.com>
2021-12-30 00:30:25 -0300

    correccion package server.js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,15 @@
+let input = document.getElementById('inputSocket');
+let parrafo = document.getElementById('description');
+let boton = document.getElementById('enviar');
+
 const socket = io.connect();
-socket.on('message', data => {
+socket.on('sala', data => {
     alert(data);
     socket.emit('notification', 'Hello from client');
+})
+input.addEventListener('onclick', (e) => {
+        console.log(input.value);
+        socket.emit('message', input.value);
+        input.value = '';        
+    
 })
--- server.js
+++ server.js
@@ -5,23 +5,31 @@ let {Server: HttpServer} = require('http');
 let app = express();
 const httpServer = new HttpServer(app);
 const io = new IOServer(httpServer);
+//io.init();
+let mensajes = [];
+let backUp = "";
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+
 app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname,'index.html');
+    res.sendFile('index.html', {root: __dirname});
 })
 httpServer.listen(3000, () => console.log('listening on port 3000'));
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.emit('message', 'Welcome to the chat app');
+    console.log(`a user connected ${socket.id}`);
+    console.log(mensajes);
     socket.on('notification', (data) => {
+        backUp = data;
+        mensajes.push({socketId: socket.id, mensaje: data});
+        io.sockets.emit('message', mensajes);
         console.log(data);
     })
+    socket.emit('sala', backUp);
 });
 
 
